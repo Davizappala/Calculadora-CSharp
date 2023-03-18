3d9a60f23f224a1e79b14095a3bd0557f0d64e20
Davi Zappala <94610458+Davizappala@users.noreply.github.com>
2023-03-18 14:07:47 -0300

    add files

---
+++ Calculadora.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, total = 0;
+            string operador;
+            Console.WriteLine("Digite o 1 numero ");
+            a = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o 2 numero ");
+            b = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Selecione a operação");
+            operador = Console.ReadLine(); 
+
+           switch (operador) 
+            {
+                case "+": 
+                    total = a + b; break;
+
+                    case "-":
+                    total = a - b;
+                    break;
+
+                    case "*":
+                    total = a * b; break;
+
+                    case "/":
+                    total = a / b; break;
+
+                
+            }
+
+            Console.WriteLine("O resultado foi: " + total);
+
+        }
+    }
+}
